064cb175142ed3e75de7fd015a9a2ea8448dc173
Connor Cozad <w0741233@myscc.ca>
2021-03-28 13:58:53 -0400

    Created the navagation items and home page of the app

--- app/src/main/java/com/example/thegamevault/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/thegamevault/ui/home/HomeFragment.java
@@ -16,14 +16,10 @@ import com.example.thegamevault.R;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        final TextView textView = root.findViewById(R.id.Title);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
